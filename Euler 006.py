2d380671719d218bacef6157204806cbb5aa4dd2
Shuang <shuangliu12@gmail.com>
2014-10-19 18:57:25 -0400

    euler 6

---
+++ Euler 006.py
@@ -0,0 +1,22 @@
+'''The sum of the squares of the first ten natural numbers is,
+12 + 22 + ... + 102 = 385
+The square of the sum of the first ten natural numbers is,
+(1 + 2 + ... + 10)2 = 552 = 3025
+Hence the difference between the sum of the squares of the first ten natural numbers and the square of the sum is 3025  385 = 2640.
+Find the difference between the sum of the squares of the first one hundred natural numbers and the square of the sum.'''
+
+
+sum_squ=0    
+for i in range(1,101):
+    sum_squ = sum_squ+i**2
+
+
+sum=0
+for j in range(1,101):
+    sum=sum+j
+    
+
+
+result = sum**2-sum_squ
+
+print result
